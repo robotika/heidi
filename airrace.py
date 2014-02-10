912d470a9faf50ae5eb4d4d94f557f529d8317e7
martind <martind@mapfactor.com>
2014-02-10 20:01:24 +0100

    extract and draw contours

--- airrace.py
+++ airrace.py
@@ -12,8 +12,10 @@ import cv2
 def processFrame( frame, debug=False ):
   gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
   ret, binary = cv2.threshold( gray, 0, 255, cv2.THRESH_OTSU )
+  contours, hierarchy = cv2.findContours( binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
   if debug:
-    cv2.imshow('image', binary)
+    cv2.drawContours(frame, contours, -1, (0,255,0), 3)
+    cv2.imshow('image', frame)
 
 def testFrame( filename ):
   img = cv2.imread( filename, 0 )
