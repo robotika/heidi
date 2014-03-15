3730a8caa512b1e3194e42cfc7b14aebfb523dfc
martind <martind@mapfactor.com>
2014-03-15 12:54:18 +0100

    implemented Pose add and sub

--- pose.py
+++ pose.py
@@ -9,3 +9,16 @@ class Pose:
 
   def __str__( self ):
     return "(%.2f, %.2f, %d)" % (self.x, self.y, math.degrees(self.heading))
+
+  def add( self, pose ):
+    x = self.x + pose.x * math.cos( self.heading ) - pose.y * math.sin( self.heading )
+    y = self.y + pose.x * math.sin( self.heading ) + pose.y * math.cos( self.heading )
+    heading = self.heading + pose.heading
+    return Pose(x, y, heading)
+
+  def sub( self, pose ):
+    heading = self.heading - pose.heading
+    x = self.x - ( pose.x * math.cos( heading ) - pose.y * math.sin( heading ) )
+    y = self.y - ( pose.x * math.sin( heading ) + pose.y * math.cos( heading ) )
+    return Pose(x, y, heading)
+
--- pose_test.py
+++ pose_test.py
@@ -7,6 +7,17 @@ class PoseTest( unittest.TestCase ):
     self.assertEqual( str(Pose()), "(0.00, 0.00, 0)" )
     self.assertEqual( str(Pose(1.234, 5, math.radians(45))), "(1.23, 5.00, 45)" )
 
+  def testAdd( self ):
+    self.assertEqual( str(Pose(1,2,math.radians(90)).add( Pose(3,4,math.radians(45))) ),
+        str(Pose( 1-4, 2+3, math.radians(90+45) )) )
+
+  def testSub( self ):
+    A = Pose(1,2,0.3)
+    B = Pose(4,5,0.6)
+    C = A.add(B)
+    self.assertEqual( str(A), str(C.sub(B)) )
+    self.assertNotEqual( str(B), str(C.sub(A)) ) # not symertic operation, like for matrices
+
 if __name__ == "__main__":
   unittest.main() 
 
