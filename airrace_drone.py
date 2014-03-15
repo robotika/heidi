b809fb37eb1cd1a6b50d95d45b08d6539e354171
martind <martind@mapfactor.com>
2014-03-15 13:01:18 +0100

    back to constant speed

--- airrace_drone.py
+++ airrace_drone.py
@@ -79,7 +79,7 @@ class AirRaceDrone( ARDrone2 ):
       self.lastImageResult = self.loggedVideoResult()
 
 
-def competeAirRace( drone, desiredSpeed = 0.4, desiredHeight = 1.5, desiredSpeedStep = 0.05 ):
+def competeAirRace( drone, desiredSpeed = 0.4, desiredHeight = 1.5, desiredSpeedStep = 0.0 ):
   loops = []
   drone.speed = 0.1
   maxVideoDelay = 0.0
