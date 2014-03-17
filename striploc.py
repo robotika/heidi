f733912843db3e4cc173acdf49204832ec3be4d0
martind <martind@mapfactor.com>
2014-03-17 07:19:09 +0100

    implemented pathType based on two following frames

--- striploc.py
+++ striploc.py
@@ -3,6 +3,7 @@
 """
 from pose import Pose
 from airrace import PATH_TURN_LEFT, PATH_TURN_RIGHT, PATH_STRAIGHT
+from ardrone2 import normalizeAnglePIPI # TODO refactoring
 import math
 
 class StripsLocalisation:
@@ -15,19 +16,34 @@ class StripsLocalisation:
   def filterPose( self, pose ):
     return pose.sub( self.basePose )
 
+  def diff2pathType( self, dx, dy, da ):
+    da = normalizeAnglePIPI(da)
+    if (0.35 < dx < 0.45) and abs(da) < math.radians(50):
+      if abs(da) < math.radians(10):
+        return PATH_STRAIGHT
+      elif da > 0:
+        return PATH_TURN_LEFT
+      else:
+        return PATH_TURN_RIGHT
+    return None
+
   def updateFrame( self, pose, frameStrips ):
-    print [str(p) for p in frameStrips]
+    print pose, [str(p) for p in frameStrips]
     for i in xrange(len(frameStrips)):
       for j in xrange(len(frameStrips)):
         if i != j:
           (dx,dy,da) = frameStrips[i].sub(frameStrips[j])
-          if (0.35 < dx < 0.45) and abs(da) < math.radians(50):
-            if abs(da) < math.radians(10):
-              self.pathType = PATH_STRAIGHT
-            elif da > 0:
-              self.pathType = PATH_TURN_LEFT
-            else:
-              self.pathType = PATH_TURN_RIGHT
+          pt = self.diff2pathType( dx, dy, da )
+          if pt:
+            self.pathType = pt
+            break
+
+    if len(frameStrips) == 1 and self.lastStripPose != None:
+      sPose = pose.add( frameStrips[0] )
+      (dx,dy,da) = sPose.sub( self.lastStripPose )
+      pt = self.diff2pathType( dx, dy, da )
+      if pt:
+        self.pathType = pt
 
     for fs in frameStrips:
       sPose = pose.add( fs )
--- striploc_test.py
+++ striploc_test.py
@@ -15,6 +15,12 @@ class StripsLocalisationTest( unittest.TestCase ):
     loc.updateFrame( Pose(), [ Pose(-0.17, 0.19, math.radians(36)), Pose(0.18, 0.37, math.radians(20))] )
     self.assertEqual( loc.pathType, PATH_TURN_RIGHT ) # note, that later this may fail do to incorrect basePose
 
+  def testFollowingFrames( self ):
+    "single strip detected in two following frames"
+    loc = StripsLocalisation()
+    loc.updateFrame( Pose(2.40, -5.13, math.radians(-74)), [Pose(-0.05, -0.04, math.radians(-8))] )
+    loc.updateFrame( Pose(2.45, -5.53, math.radians(-83)), [Pose(-0.04, -0.12, math.radians(-6))] )
+    self.assertEqual( loc.pathType, PATH_STRAIGHT )
 
 if __name__ == "__main__":
   unittest.main() 
