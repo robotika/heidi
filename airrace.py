89f756eaeadba47acb21a4cc6bf9c10c284395d8
martind <martind@mapfactor.com>
2014-03-18 06:49:33 +0100

    fixed threshold (fraction of number of pixels), refactoring frameNumber

--- airrace.py
+++ airrace.py
@@ -13,19 +13,30 @@ import numpy as np
 from pave import PaVE, isIFrame, frameNumber, timestamp
 from pose import Pose
 
+FRAMES_PER_INDEX = 15 # for simpler review of 15Hz images
+THRESHOLD_FRACTION = 0.05
+
 g_filename = None
 g_mser = None
 
 def processFrame( frame, debug=False ):
   result = []
   global g_mser
+  global THRESHOLD_FRACTION
   if g_mser == None:
     g_mser = cv2.MSER( _delta = 10, _min_area=100, _max_area=300*50*2 )
   gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
   if g_mser:
     contours = g_mser.detect(gray, None)
   else:
-    ret, binary = cv2.threshold( gray, 0, 255, cv2.THRESH_OTSU )
+    histogram = cv2.calcHist([gray],[0],None,[256],[0,256])
+    s = 0
+    for i, h in enumerate(histogram):
+      s += h
+      if s > THRESHOLD_FRACTION * 640 * 360:
+        break
+    ret, binary = cv2.threshold( gray, i, 255, cv2.THRESH_BINARY )
+#    ret, binary = cv2.threshold( gray, 0, 255, cv2.THRESH_OTSU )
     contours, hierarchy = cv2.findContours( binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE )
   for cnt in contours:
     area = cv2.contourArea(cnt, oriented=True)
@@ -141,7 +152,7 @@ def testPaVEVideo( filename, onlyFrameNumber=None ):
     pave.append( data )
     header,payload = pave.extract()
     while payload:
-      if isIFrame( header ) and (onlyFrameNumber == None or onlyFrameNumber==frameNumber( header )):
+      if isIFrame( header ) and (onlyFrameNumber == None or onlyFrameNumber==frameNumber( header )/FRAMES_PER_INDEX):
         tmpFile = open( "tmp.bin", "wb" )
         tmpFile.write( payload )
         tmpFile.flush()
@@ -151,8 +162,8 @@ def testPaVEVideo( filename, onlyFrameNumber=None ):
         assert ret
         if ret:
           global g_filename
-          g_filename = "tmp_%04d.jpg" % (frameNumber( header )/15)
-          print frameNumber( header )/15,  filterRectangles(processFrame( frame, debug=True ), minWidth=150/2)
+          g_filename = "tmp_%04d.jpg" % (frameNumber( header )/FRAMES_PER_INDEX)
+          print frameNumber( header )/FRAMES_PER_INDEX,  filterRectangles(processFrame( frame, debug=True ), minWidth=150/2)
         if onlyFrameNumber:
           cv2.waitKey(0)
           return
