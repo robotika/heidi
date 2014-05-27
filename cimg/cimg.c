f0cf903829247e0a349f46f04ed93e39d84a6458
martind <martind@mapfactor.com>
2014-05-27 22:11:12 +0200

    faster green detection using C-code with numpy array

--- cimg/cimg.c
+++ cimg/cimg.c
@@ -11,9 +11,7 @@
 static PyObject *green(PyObject *self, PyObject *args);
 
 /* .... C vector utility functions ..................*/
-PyArrayObject *pyvector(PyObject *objin);
-double *pyvector_to_Carrayptrs(PyArrayObject *arrayin);
-int  not_doublevector(PyArrayObject *vec);
+int  not_image(PyArrayObject *vec);
 
 
 /* ==== Set up the methods table ====================== */
@@ -31,58 +29,50 @@ void initcimg()  {
 
 static PyObject *green(PyObject *self, PyObject *args)
 {
-	PyArrayObject *vecin, *vecout;  // The python objects to be extracted from the args
-	double *cin, *cout;             // The C vectors to be created to point to the 
+	PyArrayObject *vecio;  // The python objects to be extracted from the args
+	unsigned char *ptr;             // The C vectors to be created to point to the 
 	                                //   python vectors, cin and cout point to the row
 	                                //   of vecin and vecout, respectively
 	int i,n;
-	const char *str;
-	double dfac;
+	double frac;
+  unsigned char r,g,b;
+  int count = 0;
 	
 	/* Parse tuples separately since args will differ between C fcns */
-	if (!PyArg_ParseTuple(args, "O!O!sd", &PyArray_Type, &vecin,
-		&PyArray_Type, &vecout, &str, &dfac))  return NULL;
-	if (NULL == vecin)  return NULL;
-	if (NULL == vecout)  return NULL;
+	if (!PyArg_ParseTuple(args, "O!d", &PyArray_Type, &vecio, &frac))  return NULL;
+	if (NULL == vecio)  return NULL;
 	
-	// Print out input string
-	printf("Input string: %s\n", str);
+	if (not_image(vecio)) return NULL;
 	
-	/* Check that objects are 'double' type and vectors
-	     Not needed if python wrapper function checks before call to this routine */
-	if (not_doublevector(vecin)) return NULL;
-	if (not_doublevector(vecout)) return NULL;
-	
-	/* Change contiguous arrays into C * arrays   */
-	cin=pyvector_to_Carrayptrs(vecin);
-	cout=pyvector_to_Carrayptrs(vecout);
+	ptr = (unsigned char*)vecio->data;
 	
 	/* Get vector dimension. */
-	n=vecin->dimensions[0];
+	n = vecio->dimensions[0] * vecio->dimensions[1];
 	
 	/* Operate on the vectors  */
-	for ( i=0; i<n; i++)  {
-			cout[i]=2.0*dfac*cin[i];
+	for( i = 0; i < n; i++ )
+  {
+    r = ptr[2];
+    g = ptr[1];
+    b = ptr[0];
+    if( g > frac * r && g > frac * b )
+    {
+      ptr[0] = 0;
+      ptr[1] = 0xFF;
+      ptr[2] = 0;
+      count++;
+    }
+    ptr += 3;
 	}
 		
-	return Py_BuildValue("i", 1);
+	return Py_BuildValue("i", count);
 }
 
 
-/* ==== Create 1D Carray from PyArray ======================
-    Assumes PyArray is contiguous in memory.             */
-double *pyvector_to_Carrayptrs(PyArrayObject *arrayin)  {
-	int n;
-	
-	n=arrayin->dimensions[0];
-	return (double *) arrayin->data;  /* pointer to arrayin data as double */
-}
-/* ==== Check that PyArrayObject is a double (Float) type and a vector ==============
-    return 1 if an error and raise exception */ 
-int  not_doublevector(PyArrayObject *vec)  {
-	if (vec->descr->type_num != NPY_DOUBLE || vec->nd != 1)  {
+int  not_image(PyArrayObject *vec)  {
+	if (vec->descr->type_num != NPY_UINT8 || vec->nd != 3)  {
 		PyErr_SetString(PyExc_ValueError,
-			"In not_doublevector: array must be of type Float and 1 dimensional (n).");
+			"In not_image: array must be of type uint8 and 3 dimensional (n).");
 		return 1;  }
 	return 0;
 }
--- green.py
+++ green.py
@@ -6,10 +6,11 @@
 """
 import sys
 import cv2
+import cimg
 
 from airrace import main as imgmain # image debugging TODO move to launcher
 
-def processGreen( frame, debug=False ):
+def processGreenVer0( frame, debug=False ):
   for y in xrange(frame.shape[0]):
     for x in xrange(frame.shape[1]):
       b,g,r = frame[y][x]
@@ -17,6 +18,10 @@ def processGreen( frame, debug=False ):
         frame[y][x] = (0, 255, 0)
   cv2.imshow('image', frame)
 
+def processGreen( frame, debug=False ):
+  cimg.green( frame, 1.1 )
+  cv2.imshow('image', frame)
+
 if __name__ == "__main__":
   imgmain( sys.argv, processGreen )
-
+ 
